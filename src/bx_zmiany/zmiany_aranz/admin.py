7c8908dc0e9200262746fa019185723720e20091
Grzegorz Świtek <gswitek@budlex.local>
2022-12-23 12:19:54 +0100

    Add Procedure to Admin site

--- src/bx_zmiany/zmiany_aranz/admin.py
+++ src/bx_zmiany/zmiany_aranz/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from zmiany_aranz.models import Procedure
+
+
+class ProcedureAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Procedure, ProcedureAdmin)
