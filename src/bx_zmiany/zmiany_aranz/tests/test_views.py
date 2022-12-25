a35262b3a8c618c85bd5b6316dbd67df156feca4
gswitek <switek@budlex.pl>
2022-12-26 00:05:16 +0100

    Add test to procedure view

--- src/bx_zmiany/zmiany_aranz/tests/test_views.py
+++ src/bx_zmiany/zmiany_aranz/tests/test_views.py
@@ -1,1 +1,19 @@
 from django.urls import reverse
+from django.test import TestCase
+from django.test.utils import setup_test_environment
+
+from zmiany_aranz.models import Procedure
+
+
+class ProcedureDetailViewTests(TestCase):
+    def test_procedure_status_200(self):
+        procedure = Procedure.objects.create()
+        response = self.client.get(
+            reverse(
+                "zmiany_aranz:procedure_detail_view",
+                kwargs={"pk": procedure.pk},
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "/")
+        self.assertEqual(response.context["object"], procedure)
--- src/bx_zmiany/zmiany_aranz/urls.py
+++ src/bx_zmiany/zmiany_aranz/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from zmiany_aranz.views import ProcedureDetailView
 
+app_name = "zmiany_aranz"
+
 urlpatterns = [
     path(
-        "procedure/<int:pk>",
+        "procedure/<int:pk>/",
         ProcedureDetailView.as_view(),
         name="procedure_detail_view",
     ),
