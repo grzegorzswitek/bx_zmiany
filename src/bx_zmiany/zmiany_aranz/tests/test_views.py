6df01ef824e442cd69b0a92e08f2fdee437aba3b
gswitek <switek@budlex.pl>
2022-12-31 00:19:54 +0100

    Add simple template for procedure (detail)

--- src/bx_zmiany/zmiany_aranz/tests/test_views.py
+++ src/bx_zmiany/zmiany_aranz/tests/test_views.py
@@ -6,7 +6,7 @@ from zmiany_aranz.models import Procedure
 
 
 class ProcedureDetailViewTests(TestCase):
-    def test_procedure_status_200(self):
+    def test_procedure_status_code_200(self):
         procedure = Procedure.objects.create()
         response = self.client.get(
             reverse(
@@ -17,3 +17,15 @@ class ProcedureDetailViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "/")
         self.assertEqual(response.context["object"], procedure)
+
+    def test_procedure_status_code_404(self):
+        procedure = Procedure.objects.create()
+        response = self.client.get(
+            reverse(
+                "zmiany_aranz:procedure_detail_view",
+                kwargs={"pk": procedure.pk + 1},
+            )
+        )
+        self.assertEqual(response.status_code, 404)
+        print(response.content)
+        # self.assertContains(response, "/")
