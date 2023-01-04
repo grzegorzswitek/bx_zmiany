c2e55c53a9e502c094f7dfa1e75dfc93a4320734
gswitek <switek@budlex.pl>
2023-01-04 19:38:09 +0100

    Add a invoice subpages for procedure #4

--- src/bx_zmiany/zmiany_aranz/tests/test_views.py
+++ src/bx_zmiany/zmiany_aranz/tests/test_views.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from django.test import TestCase
 from django.test.utils import setup_test_environment
 
-from zmiany_aranz.models import Procedure, Cost, KindOfCost
+from zmiany_aranz.models import Procedure, Cost, KindOfCost, Invoice
 
 
 class ProcedureDetailViewTests(TestCase):
@@ -182,6 +182,153 @@ class CostUpdateTest(TestCase):
     def test_update_POST(self):
         response = self.client.post(
             reverse("zmiany_aranz:cost_update", kwargs={"pk": self.cost.pk}),
+            {
+                "net": 200,
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "200")
+
+
+class ProcedureInvoicesListTests(TestCase):
+    def setUp(self) -> None:
+        self.invoice_1 = Invoice.objects.create(
+            number="FVS/1/1/2022",
+            invoice_date="2022-01-01",
+            due_date="2022-01-15",
+            net=100,
+            vat=8,
+            gross=108,
+            description="",
+        )
+        self.invoice_2 = Invoice.objects.create(
+            number="FVS/2/1/2022",
+            invoice_date="2022-01-02",
+            due_date="2022-01-16",
+            net=100,
+            vat=8,
+            gross=108,
+            description="",
+        )
+        self.procedure_with_invoices = Procedure.objects.create()
+        self.procedure_without_invoices = Procedure.objects.create()
+        self.procedure_with_invoices.invoices.set([self.invoice_1, self.invoice_2])
+
+    def test_procedure_without_invoices(self):
+        response = self.client.get(
+            reverse(
+                "zmiany_aranz:procedure_invoices_list",
+                kwargs={"pk": self.procedure_without_invoices.pk},
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Brak faktur.")
+
+    def test_procedure_with_invoices(self):
+        response = self.client.get(
+            reverse(
+                "zmiany_aranz:procedure_invoices_list",
+                kwargs={"pk": self.procedure_with_invoices.pk},
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "FVS/2/1/2022")
+
+    def test_procedure_does_not_exist(self):
+        response = self.client.get(
+            reverse("zmiany_aranz:procedure_invoices_list", kwargs={"pk": 0})
+        )
+        self.assertEqual(response.status_code, 404)
+
+
+class ProcedureInvoiceCreateTests(TestCase):
+    def setUp(self) -> None:
+        self.procedure = Procedure.objects.create()
+        self.invoice = Invoice.objects.create(
+            number="FVS/1/1/2022",
+            invoice_date="2022-01-01",
+            due_date="2022-01-15",
+            net=100,
+            vat=8,
+            gross=108,
+            description="",
+        )
+        self.procedure.invoices.set([self.invoice])
+
+    def test_create_invoice(self):
+        response = self.client.post(
+            reverse(
+                "zmiany_aranz:procedure_invoice_create",
+                kwargs={"pk": self.procedure.pk},
+            ),
+            {
+                "number": "FVS/3/2/2022",
+                "invoice_date": "2022-02-01",
+                "due_date": "2022-02-15",
+                "net": 1000,
+                "vat": 80,
+                "gross": 1080,
+            },
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Invoice.objects.last().number, "FVS/3/2/2022")
+
+
+class InvoiceDeleteTest(TestCase):
+    def setUp(self) -> None:
+        self.procedure = Procedure.objects.create()
+        self.invoice = Invoice.objects.create(
+            number="FVS/1/1/2022",
+            invoice_date="2022-01-01",
+            due_date="2022-01-15",
+            net=100,
+            vat=8,
+            gross=108,
+            description="",
+        )
+        self.procedure.invoices.set([self.invoice])
+
+    def test_delete_confirmation_page(self):
+        response = self.client.get(
+            reverse("zmiany_aranz:invoice_delete", kwargs={"pk": self.invoice.pk})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Czy na pewno usunąć fakturę?")
+
+    def test_delete_success_page(self):
+        response = self.client.post(
+            reverse("zmiany_aranz:invoice_delete", kwargs={"pk": self.invoice.pk}),
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Brak faktur.")
+
+
+class InvoiceUpdateTest(TestCase):
+    def setUp(self) -> None:
+        self.procedure = Procedure.objects.create()
+        self.invoice = Invoice.objects.create(
+            number="FVS/1/1/2022",
+            invoice_date="2022-01-01",
+            due_date="2022-01-15",
+            net=100,
+            vat=8,
+            gross=108,
+            description="",
+        )
+        self.procedure.invoices.set([self.invoice])
+
+    def test_update_GET(self):
+        response = self.client.get(
+            reverse("zmiany_aranz:invoice_update", kwargs={"pk": self.invoice.pk})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "100")
+
+    def test_update_POST(self):
+        response = self.client.post(
+            reverse("zmiany_aranz:invoice_update", kwargs={"pk": self.invoice.pk}),
             {
                 "net": 200,
             },
--- src/bx_zmiany/zmiany_aranz/views.py
+++ src/bx_zmiany/zmiany_aranz/views.py
@@ -10,7 +10,7 @@ from django.views.generic import (
 )
 from django.urls import reverse
 
-from .models import Procedure, Cost
+from .models import Procedure, Cost, Invoice
 
 
 class ProcedureDetailView(DetailView):
@@ -90,3 +90,80 @@ class CostUpdateView(UpdateView):
     def get_success_url(self) -> str:
         procedure = self.object.procedures.first()
         return reverse("zmiany_aranz:procedure_costs_list", kwargs={"pk": procedure.pk})
+
+
+class ProcedureInvoicesList(ListView):
+    model = Invoice
+    template_name = "zmiany_aranz/procedure_invoices_list.html"
+
+    def get_queryset(self):
+        queryset = super(ProcedureInvoicesList, self).get_queryset()
+        pk = self.kwargs["pk"]
+        queryset = queryset.filter(procedures__in=[pk])
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(ProcedureInvoicesList, self).get_context_data(**kwargs)
+        pk = self.kwargs["pk"]
+        try:
+            context["procedure"] = Procedure.objects.get(pk=pk)
+        except Procedure.DoesNotExist:
+            context["procedure"] = None
+        return context
+
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs["pk"]
+        try:
+            Procedure.objects.get(pk=pk)
+        except Procedure.DoesNotExist:
+            raise Http404
+        return super().get(request, *args, **kwargs)
+
+
+class InvoiceCreateView(CreateView):
+    model = Invoice
+    fields = "__all__"
+    template_name = "zmiany_aranz/procedure_invoice_create.html"
+
+    def get_success_url(self) -> str:
+        pk = self.kwargs.get("pk", None)
+        if pk is None:
+            return super().get_success_url()
+        return reverse("zmiany_aranz:procedure_invoices_list", kwargs={"pk": pk})
+
+    def form_valid(self, form):
+        response = super(InvoiceCreateView, self).form_valid(form)
+        pk = self.kwargs.get("pk", None)
+        if pk is None:
+            raise Http404
+        try:
+            procedure = Procedure.objects.get(pk=pk)
+        except Procedure.DoesNotExist:
+            return Http404
+        procedure.invoices.add(self.object)
+        return response
+
+
+class InvoiceDeleteView(DeleteView):
+    model = Invoice
+    template_name = "zmiany_aranz/invoice_delete_confirm.html"
+
+    def get_success_url(self) -> str:
+        procedure = self.object.procedures.first()
+        if procedure is None:
+            return "/"
+        return reverse(
+            "zmiany_aranz:procedure_invoices_list", kwargs={"pk": procedure.pk}
+        )
+
+
+class InvoiceUpdateView(UpdateView):
+    model = Invoice
+    fields = "__all__"
+    template_name = "zmiany_aranz/invoice_update.html"
+
+    def get_success_url(self) -> str:
+        procedure = self.object.procedures.first()
+        return reverse(
+            "zmiany_aranz:procedure_invoices_list", kwargs={"pk": procedure.pk}
+        )
