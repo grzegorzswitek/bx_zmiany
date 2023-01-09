6a6b862d27971cbecf5acb1dbf35bec39054efb3
gswitek <switek@budlex.pl>
2023-01-09 13:24:31 +0100

    Regenerate settings.SECRET_KEY

--- src/bx_zmiany/bx_zmiany/settings.py
+++ src/bx_zmiany/bx_zmiany/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-57rvey4flmd0*e6pb43=sv^r4nzk@czt(58+s6be5wvj-(&7p9"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
