da9f84809c493ccea8ec67f0b269c10d739e18fc
gswitek <switek@budlex.pl>
2023-02-05 11:57:06 +0100

    Add the admin panel to the main menu.

--- src/bx_zmiany/bx_zmiany/urls.py
+++ src/bx_zmiany/bx_zmiany/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = (
     [
         path("", include("zmiany_aranz.urls")),
-        path("admin/", admin.site.urls),
+        path("admin/", admin.site.urls, name="admin"),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
